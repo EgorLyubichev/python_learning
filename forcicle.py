b1e5c6d08c8b07074b999f1046713a9e78b8da68
Egor Lyubichev <elyubichev@alfabank.ru>
2024-03-18 14:50:33 +0300

    added sth

---
+++ forcicle.py
@@ -0,0 +1,13 @@
+# Simple example:
+for i in range(4):
+    print(i)
+
+from random import randint
+
+for j in range(5):
+    a = randint(1, 100)
+    print(a)
+
+a = [42, 13, 25, 14, 7]
+for i in a:
+    print('i: ', i, end=' ')
